49180b5f4ef91effd842e41e6f5ca72845497d24
pwn <fabian.swinne@outlook.de>
2025-10-28 12:38:28 +0100

    Blatt 2 Aufgabe 1

--- Source/Aufgabe2_1.s
+++ Source/Aufgabe2_1.s
@@ -11,9 +11,57 @@
 .text /* Specify that code goes in text segment */
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
+/*
+wie gegeben
+.equ BREAD_bm, (1<<0)
+.equ BUTTER_bm, (1<<1)
+.equ CHEESE_bm, (1<<2)
+
+.equ TEQUILA_bm, (1<<8)
+.equ MILK_bm, (1<<9)
+.equ RUM_bm, (1<<10)
+.equ VINE_bm, (1<<11)
+.equ VODKA_bm, (1<<12)
+
+.equ ALMOND_bm, (1<<8)
+.equ PEANUT_bm, (1<<9)
+.equ CHESTNUTS_bm, (1<<10)
+
+.equ APPLE_bm, (1<<12)
+.equ MANGO_bm, (1<<13)
+.equ LEMON_bm, (1<<14)
+*/
+//gefixt
+.equ BREAD_bm, (1<<0)
+.equ BUTTER_bm, (1<<1)
+.equ CHEESE_bm, (1<<2)
+.equ BASICS_bm, (BREAD_bm|BUTTER_bm|CHEESE_bm)
+
+.equ TEQUILA_bm, (1<<3)
+.equ MILK_bm, (1<<4)
+.equ RUM_bm, (1<<5)
+.equ VINE_bm, (1<<6)
+.equ VODKA_bm, (1<<7)
+.equ DRINKS_bm, (TEQUILA_bm|MILK_bm|RUM_bm|VINE_bm|VODKA_bm)
+
+.equ ALMOND_bm, (1<<8)
+.equ PEANUT_bm, (1<<9)
+.equ CHESTNUTS_bm, (1<<10)
+.equ NUTS_bm, (ALMOND_bm|PEANUT_bm|CHESTNUTS_bm)
+
+.equ APPLE_bm, (1<<11)
+.equ MANGO_bm, (1<<12)
+.equ LEMON_bm, (1<<13)
+.equ FRUITS_bm, (APPLE_bm|MANGO_bm|LEMON_bm)
+
+.equ BRAKFAST_bm, (BREAD_bm|BUTTER_bm|CHEESE_bm|MILK_bm|PEANUT_bm|LEMON_bm)
+
 main:
-
-
+        mov R0, BREAKFAST_bm
+        mov R1, BREAKFAST_bm|NUTS_bm
+        mov R2, (BREAKFAST_bm & ~MILK_bm)|VINE_bm
+        mov R3, FRUITS_bm|MILK_bm|(MILK_bm<<16)
+        mov R4, (BREAKFAST_bm & ~BUTTER_bm)
 stop:
 	nop
 	bal stop
