53efbeba52c4178cbfade1881e85e2f2decd87ce
pwn <fabian.swinne@outlook.de>
2025-10-30 10:39:49 +0100

    Blatt 2 Aufgabe 2

--- Source/Aufgabe2_2.s
+++ Source/Aufgabe2_2.s
@@ -13,6 +13,22 @@
 .global main /* Specify global symbol */
 main:
 
+        // Faktoren
+        movs R0, #5
+        beq stop
+        movs R1, #4
+        beq stop // Ergebnis ist 0 wenn einer der Faktoren 0 ist
+
+        // Ergebnis
+        mov R2, #0
+        // Überlauf
+        mov R3, #0
+
+while:
+        adds R2, R1 // R2 += R1 <> R0 mal
+        addvs R3, #1 // wenn überlauf R3++
+        subs r0, #1
+        bne while
 
 stop:
 	nop
--- Source/Aufgabe2_3.s
+++ Source/Aufgabe2_3.s
@@ -11,8 +11,28 @@
 .text /* Specify that code goes in text segment */
 .code 32 /* Select ARM instruction set */
 .global main /* Specify global symbol */
+.equ THRESHHOLD, 80
 main:
 
+        mov R0, #0
+        mov R1, #0
+        mov R2, #0
+        mov R3, #0
+        mov R4, #0
+        mov R5, #0
+        mov R6, #0
+        mov R7, #0
+
+        mov R8, #0
+
+        cmp R0, THRESHHOLD
+        cmp R1, THRESHHOLD
+        cmp R2, THRESHHOLD
+        cmp R3, THRESHHOLD
+        cmp R4, THRESHHOLD
+        cmp R5, THRESHHOLD
+        cmp R6, THRESHHOLD
+        cmp R7, THRESHHOLD
 
 stop:
 	nop
